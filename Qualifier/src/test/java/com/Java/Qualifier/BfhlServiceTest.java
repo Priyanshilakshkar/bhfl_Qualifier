615fc254e7015e7ffe190c602196ea8f100fa618
Priyanshilakshkar <168474934+Priyanshilakshkar@users.noreply.github.com>
2026-05-26 15:48:01 +0530

    Added Docker support

--- Qualifier/src/test/java/com/Java/Qualifier/BfhlServiceTest.java
+++ Qualifier/src/test/java/com/Java/Qualifier/BfhlServiceTest.java
@@ -1,5 +1,30 @@
 package com.Java.Qualifier;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import com.Java.Qualifier.Dto.BfhlRequest;
+import com.Java.Qualifier.Dto.BfhlResponse;
+import com.Java.Qualifier.Service.BfhlServiceImpl;
+import org.junit.jupiter.api.Test;
+
 public class BfhlServiceTest {
 
+    @Test
+    void testProcessData() {
+
+        BfhlServiceImpl service = new BfhlServiceImpl();
+
+        BfhlRequest request = new BfhlRequest();
+
+        request.setData(List.of("1", "2", "a", "&"));
+
+        BfhlResponse response = service.processData(request);
+
+        assertTrue(response.is_success());
+
+        assertEquals("3", response.getSum());
+    }
 }
